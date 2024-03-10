af4ff5dbd8c66bec7d4a90e8b2aa2063dcae5849
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2024-03-10 13:50:21 -0400

    updates the test route file

--- app/api/create-pets-table/route.ts
+++ app/api/create-pets-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE tapes ( barcode text, title varchar(255), description text, genre varchar(255), year int );`;
+      await sql`CREATE TABLE tapes ( id SERIAL PRIMARY KEY, barcode text, title varchar(255), description text, genre varchar(255), year int, coverfront bytea );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
