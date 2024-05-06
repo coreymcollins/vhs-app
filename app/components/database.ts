df9cdf8cf38533312ab71fe805ec13136f09ea6e
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2024-05-06 18:29:55 -0400

    removes unneeded files

--- app/components/database.ts
+++
@@ -1,9 +0,0 @@
-import postgres from 'postgres';
-
-const sslSetting = 'development' === process.env.NODE_ENV ? false : 'allow';
-
-let sql = postgres(process.env.DATABASE_URL || process.env.POSTGRES_URL!, {
-    ssl: sslSetting,
-});
-
-export default sql
