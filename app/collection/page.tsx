4a738f831784337b752ac3093b96fda2c196f568
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2024-05-25 21:14:29 -0400

    adds collection URL button back

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from '@/utils/supabase/server';
 import { checkLoginStatus } from '../actions/check-login-status';
 import { WithPagination } from '../components/with-pagination';
+import { getUsernameByUuid } from '../actions';
+import CopyCollectionUrl from '@/app/components/button-copy-collection-url';
 
 async function getUsersTapes() {
     const supabase = createClient()
@@ -23,6 +25,7 @@ async function getUsersTapes() {
 export default async function LibraryPage( req: any ) {
     const tapes = await getUsersTapes()
     const userAuth = await checkLoginStatus()
+    const username = await getUsernameByUuid( userAuth.id )
     
     const totalTapes = tapes.length
     let { page } = req.searchParams
@@ -32,8 +35,9 @@ export default async function LibraryPage( req: any ) {
         <>
             <div className="page-content-header">
                 <h2>My Library ({totalTapes})</h2>
+                <CopyCollectionUrl username={username} />
             </div>
-
+            
             { null !== tapes && <WithPagination tapes={tapes} session={userAuth} pageNumber={page} /> }
         </>
     )
